af330c60e49a37c90ead5e28429f2c89caeed10c
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-06-05 16:42:00 +0700

    up shopper cart

---
+++ shopper_cart/lib/routers/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:go_router/go_router.dart';
+import 'package:shopper_cart/screens/cart_screen.dart';
+import 'package:shopper_cart/screens/login_screen.dart';
+import 'package:shopper_cart/screens/product_screen.dart';
+
+GoRouter router() {
+  return GoRouter(initialLocation: '/login', routes: [
+    GoRoute(
+      path: '/login', 
+      builder: (context, state) => LoginScreen()
+    ),
+    GoRoute(
+      path: '/product', 
+      builder: (context, state) => ProductScreen()
+    ),
+    GoRoute(
+      path: '/cart', 
+      builder: (context, state) => CartScreen()
+    ),
+  ]);
+}
