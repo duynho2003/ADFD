b1f1c055063f1acc6821dca4d8611193804c67e3
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-06-06 13:37:02 +0700

    up login shopper

--- shopper_cart/lib/screens/login_screen.dart
+++ shopper_cart/lib/screens/login_screen.dart
@@ -8,8 +8,72 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  var _formKey = GlobalKey<FormState>(); // Form validation key
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  prefixIcon: Icon(Icons.person),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your username';
+                  } else if (value.length < 3) {
+                    return 'Username must at least 3 chars';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.length < 8) {
+                    return 'Password must at least 8 chars';
+                  } else if (!value.contains(RegExp('[0-9]'))) {
+                    return 'Password must have 1 numberic char';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 24.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Perform login logic here
+                    print('Perform login: call login api');
+                    // ... (Authenticate user, navigate, etc.)
+                  } else {
+                    print('Check validation failed');
+                  }
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
